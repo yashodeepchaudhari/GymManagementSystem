cbab81992dc80deab3f04afa9d5018111daf3fda
Ubuntu <ubuntu@ip-172-31-36-229.ap-south-1.compute.internal>
2026-03-30 19:00:26 +0000

    while deploying changes

--- GymManagementSystem/myproject/myproject/urls.py
+++ GymManagementSystem/myproject/myproject/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Static files
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+#urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
